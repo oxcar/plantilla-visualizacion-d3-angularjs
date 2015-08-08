3f1a0be93f94cebe8977f2a9a1ab5e54036432d9
Oscar Hernandez <oxcarh@gmail.com>
2015-08-07 23:38:36 -0500

    Initial commit

---
+++ js/viz.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var app = angular.module('VizApp', []);
+
+app.controller('VizController', ['$scope', '$rootScope', '$http', '$compile', function ($scope, $rootScope, $http, $compile) {
+
+    $scope.init = function () {
+
+    };
+
+}]);
